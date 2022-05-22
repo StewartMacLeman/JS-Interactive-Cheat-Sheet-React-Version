c02ad7fb6179d2d744b8de6e2734bbd606a4564a
Stewart MacLeman <stewart_macleman@hotmail.com>
2022-05-22 12:58:25 +0100

    Navigation works.

--- src/layout_components/Header.js
+++ src/layout_components/Header.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import Navigation from "./Navigation";
 
 const Header = () => {
+
+  const [pageTitle, setPageTitle] = useState("Home");
+
+  const getTitle = (e) => {
+    let itemClicked = e.target;
+    let title = itemClicked.textContent;
+    setPageTitle(title);
+  };
+
+
+  const showNavigation = (e) => {
+    let nextElement = e.target.nextElementSibling;
+    nextElement.classList.toggle("hide");
+
+    let button = e.target;
+    let buttonText = e.target.textContent;
+    let hide = "Hide Nav";
+    let show = "Show Nav";
+
+    if (buttonText === "Show Nav") {
+      button.textContent = hide;
+    } else if (buttonText === "Hide Nav") {
+      button.textContent = show;
+    }
+  };
+
   return (
     <header className="header">
-      <h1>JS Cheat-Sheet: Home</h1>
-      <button type="button" className="navButton">
+      <h1>JS Cheat-Sheet: {pageTitle}</h1>
+      <button type="button" onClick={showNavigation}>
         Show Nav
       </button>
-      <Navigation />
+      <Navigation getTitle={getTitle}/>
     </header>
   );
 };
--- src/layout_components/Navigation.js
+++ src/layout_components/Navigation.js
@@ -1,46 +1,45 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const Navigation = () => {
+const Navigation = ({getTitle}) => {
 const active = ({ isActive }) => isActive ? "active" : "";
   return (
-    <nav className="navigation">
-        {/* The hide class to be added! */}
+    <nav className="navigation hide">
       <ul>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="/" className={active}>Home</NavLink>
         </li>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="01_typeof" className={active}>The "typeof" Operator</NavLink>
         </li>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="02_arithmetic_ops" className={active}>Arithmetic Operators</NavLink>
         </li>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="03_comparison_ops" className={active}>Comparison Operators</NavLink>
         </li>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="04_if_etc" className={active}>"if" Statements etc.</NavLink>
         </li>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="05_for_etc" className={active}>"for" Statements etc.</NavLink>
         </li>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="06_strings" className={active}>Strings</NavLink>
         </li>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="07_numbers" className={active}>Numbers</NavLink>
         </li>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="08_dates" className={active}>Dates</NavLink>
         </li>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="09_arrays" className={active}>Arrays</NavLink>
         </li>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="10_objects" className={active}>Objects</NavLink>
         </li>
-        <li>
+        <li onClick={getTitle}>
           <NavLink to="11_functions" className={active}>Functions</NavLink>
         </li>
       </ul>
